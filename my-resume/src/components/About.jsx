41a0c214d157272ed4ec4f8bbdabef5d01a15806
rekhanthms <69860222+rekhanthms@users.noreply.github.com>
2024-08-12 17:54:03 +0530

    About

---
+++ my-resume/src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const About=()=>{
+    return (
+        <section>
+            <h1>
+                Madhurantakam Somasekhara Rekhanth
+            </h1>
+            <h4>
+                <i>• rekhanthms23@gmail.com • 7032366733 • https://www.linkedin.com/in/rekhanth-m-s-22344818b/</i>
+
+            </h4>
+            <a>
+                I bring robust expertise in Java, MS SQL, .NET, and Angular with hands-on experience in developing scalable web applications. As a
+                Backend developer, I played a key role in creating efficient and high-performance solutions. My dedication to solving complex problems
+                and delivering top-tier software solutions is evident in my work.
+            </a>
+        </section>
+        
+    );
+};
+
+export default About;
